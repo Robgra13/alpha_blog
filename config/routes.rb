908625ab6dfb1b33da66aaf5445ac65e21943867
Robert Gradowski <rgradowski@wp.eu>
2023-07-12 18:03:35 +0200

    final version

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to:'pages#about'
-  get 'articles', to: 'articles#index'
+  #get 'articles', to: 'articles#index'
 
-  resources :articles, only: [:show, :index]
+  resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
