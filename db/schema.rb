6b97b19a3412b34f4b661a706493755a61d1444c
Robert Gradowski <rgradowski@wp.eu>
2023-07-14 00:10:08 +0200

    database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_10_102513) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_13_212429) do
   create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
